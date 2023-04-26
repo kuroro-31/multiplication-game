cdebdbce294737ddb9a65fba6361595411f3da90
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-04-27 02:01:00 +0900

    ファイル追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+
+import type { AppProps } from 'next/app';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
+};
