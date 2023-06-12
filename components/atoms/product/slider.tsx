7aa2bd32d1fb4341f4bc946d5b3ad5b9add7b9e6
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-06-12 18:59:59 +0900

    slider メイン画像の縦横中央

--- components/atoms/product/slider.tsx
+++ components/atoms/product/slider.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 
 interface ImageSliderProps {
   images: string[]; // 画像のURLのリスト
@@ -6,11 +6,33 @@ interface ImageSliderProps {
 
 const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0); // 現在の画像のインデックス
+  const [zoomPosition, setZoomPosition] = useState({ x: 0, y: 0 }); // マウスの位置
+  const [showZoom, setShowZoom] = useState(false); // ズーム表示を管理
+  const imageRef = useRef<HTMLImageElement>(null);
 
   // サムネイルがクリックされたときのハンドラ
   const handleThumbnailClick = useCallback((index: number) => {
     setCurrentIndex(index);
   }, []);
+
+  // マウスの位置を更新する
+  const handleMouseMove = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    if (imageRef.current) {
+      const rect = imageRef.current.getBoundingClientRect();
+      setZoomPosition({
+        x: (event.clientX - rect.left) / rect.width,
+        y: (event.clientY - rect.top) / rect.height,
+      });
+      setShowZoom(true);
+    }
+  };
+
+  // マウスが画像外に出たらズームを隠す
+  const handleMouseLeave = () => {
+    setShowZoom(false);
+  };
 
   return (
     <div className="flex m-8">
@@ -29,12 +51,30 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
           </div>
         ))}
       </div>
-      <div className="lg:h-[668px] lg:w-[668px] ml-4">
+      <div
+        className="relative lg:h-[668px] lg:w-[668px] ml-4 flex items-center justify-center"
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+      >
         <img
+          ref={imageRef}
           src={images[currentIndex]}
           alt=""
           className="max-w-full max-h-full object-contain"
         />
+        {showZoom && (
+          <div
+            style={{
+              backgroundImage: `url(${images[currentIndex]})`,
+              backgroundPosition: `${zoomPosition.x * 100}% ${
+                zoomPosition.y * 100
+              }%`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "200%",
+            }}
+            className="absolute top-0 left-[100%] h-[200px] w-[200px] grid bg-primary bg-opacity-20"
+          />
+        )}
       </div>
     </div>
   );
